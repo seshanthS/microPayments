05221f7b1c88e35c115cda7fcd30ac1dc13b133a
seshanthS <seshanth.s@hotmail.com>
2019-01-21 21:49:36 +0530

    ethereumjs-tx to sign and send transaction

--- ui/routes/index.js
+++ ui/routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var Web3 = require('web3');
-var web3Functions = require('./functions.js')
-var walletProvider = require('truffle-hdwallet-provider');
+var Tx = require('ethereumjs-tx')
+var web3Functions = require('./functions.js');
 var abi = require('./abi.js');
 var router = express.Router();
-var provider = "";
+var httpProviderUrl = "";
 var contractAddress = ""
 
 /* GET home page. */
@@ -15,19 +15,41 @@ router.get('/', function(req, res, next) {
 	/createChannel - send keystore of sender, receivers's address,amount in ethers
 */
 router.post('/createChannel',(req,res,next)=>{
+  //creds contains the decrypted keystore.
   var creds = req.body.creds;
   //amount in ethers, i.e amount = 2; 2 ethers nu koduka venam.
   var amount = creds.amount;
   var keyStore = creds.keyStore;
   var receiver = creds.receiver;
-
-  var web3 = new Web3(Web3.providers.HttpProvider(provider));
+  var password = creds.password;
+  var web3 = new Web3(Web3.providers.HttpProvider(httpProviderUrl));
 
   var amountInWei = web3.utils.toWei(amount);
   var contractInstance = new web3.eth.Contract(abi,contractAddress);
-  contractInstance.methods.createChannel(sender, receiver).send({from:sender, value: amountInWei}).then(receipt=>{
-  	//receipt also contains the returned channelId from contract;
-  	res.send(receipt);
+  var txData = contractInstance.methods.createChannel(sender, receiver);
+  var txDataEncoded = txData.encodeABI();
+
+  web3.eth.accounts.decrypt(keyStore, password).then(decryptedAccount=>{
+  	var sender = decryptedAccount.address;
+  	var key = new Buffer(decryptedAccount.privateKey, 'hex');
+  	web3.eth.getTransactionCount(sender).then(txCount=>{
+  	var rawTx = {
+  		from: sender,
+  		to: contractAddress,
+  		nonce: count,
+  		data: txDataEncoded,
+  		gasPrice: 210000,
+        gasLimit: 60000000000,
+  	}
+  	var tx = new Tx(rawTx);
+  	tx.sign(key);
+
+  	var serializedTx = tx.serialize();
+  	web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('receipt',(receipt)=>{
+  		res.send(receipt);
+  	});
+
+	});
   });
 });
 
@@ -40,7 +62,7 @@ router.post('/signTransaction',(req,res,next)=>{
   var amount = creds.amount;
   var key = creds.key;
 
-  var web3 = new Web3(Web3.providers.HttpProvider(provider));
+  var web3 = new Web3(Web3.providers.HttpProvider(httpProviderUrl));
 
   var amountWei = web3.utils.toWei(amount);
   
@@ -50,6 +72,8 @@ router.post('/signTransaction',(req,res,next)=>{
 
 router.get('/withdraw',(req,res,next)=>{
   var data = req.body.data;
+  var keyStore = data.keyStore;
+  var password = creds.password;
   var channelId = data.channelId;
   var amountHash = data.amountHash;
   var amount = data.amount;	
@@ -58,31 +82,70 @@ router.get('/withdraw',(req,res,next)=>{
   var s = data.s;
   var v = data.v;
 
-  var web3 = new Web3(Web3.providers.HttpProvider(provider));
   var contractInstance = new web3.eth.Contract(abi, contractAddress);
-
+  var web3 = new Web3(Web3.providers.HttpProvider(httpProviderUrl));
   /*
     TODO :
      .on('transactionHash'){ updateUi to waiting};
      .on('receipt'){send receipt};
   */
-  contractInstance.methods.withdraw(channelId, amountHash, amount, signature, r, s, v).send({from:fromAddress}).then(receipt=>{
-  	console.log(receipt);
-  	res.send(receipt);
-  })
-});
+  var txData = contractInstance.methods.withdraw(channelId, amountHash, amount, signature, r, s, v);
+  var txDataEncoded = txData.encodeABI();
+
+  web3.eth.accounts.decrypt(keyStore, password).then(decryptedAccount=>{
+  	var sender = decryptedAccount.address;
+  	var key = new Buffer(decryptedAccount.privateKey, 'hex');
+  	web3.eth.getTransactionCount(sender).then(txCount=>{
+  	var rawTx = {
+  		from: sender,
+  		to: contractAddress,
+  		nonce: count,
+  		data: txDataEncoded,
+  		gasPrice: 210000,
+        gasLimit: 60000000000,
+  	}
+  	var tx = new Tx(rawTx);
+  	tx.sign(key);
+
+  	var serializedTx = tx.serialize();
+  	web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('receipt',(receipt)=>{
+  		res.send(receipt);
+  	});
+
+	});
+  });
 
 router.post('/addEther',(req, res, next)=>{
-	var creds = req.body.creds;
+  var creds = req.body.creds;
+  var provider = new walletProvider(key, httpProviderUrl)
+  var web3 = new Web3(Web3.providers.HttpProvider(provider));
+  var contractInstance = new web3.eth.Contract(abi, contractAddress);
+  var fromAddress = provider.getAddresses();
 
-});
+  var txData = contractInstance.methods.addEtherToChannel(channelId);
+  var txDataEncoded = txData.encodeABI();
 
-router.get('/test',(req,res,next)=>{
-	var fromA = 
-	var toB =
-	var web3 = new Web3(provider);
+  web3.eth.accounts.decrypt(keyStore, password).then(decryptedAccount=>{
+  	var sender = decryptedAccount.address;
+  	var key = new Buffer(decryptedAccount.privateKey, 'hex');
+  	web3.eth.getTransactionCount(sender).then(txCount=>{
+  	var rawTx = {
+  		from: sender,
+  		to: contractAddress,
+  		nonce: count,
+  		data: txDataEncoded,
+  		gasPrice: 210000,
+        gasLimit: 60000000000,
+  	}
+  	var tx = new Tx(rawTx);
+  	tx.sign(key);
 
+  	var serializedTx = tx.serialize();
+  	web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')).on('receipt',(receipt)=>{
+  		res.send(receipt);
+  	});
 
+	});
 
 })
 
