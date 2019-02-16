4d30d59a626bd7aafe27f04eed02e2a4f9496d4f
seshanthS <seshanth.s@hotmail.com>
2019-02-16 16:17:13 +0000

    Changed endpoints to https

--- server/app.js
+++ server/app.js
@@ -2,19 +2,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var fs = require('fs');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-var http1 = require('http')//.Server(app)
-var server = http1.createServer(app)
+var options = {
+  key: fs.readFileSync('certs/key.pem'),
+  cert: fs.readFileSync('certs/certificate.pem')
+};
+
+var http1 = require('https')//.Server(app)
+var server = http1.createServer(options,app)
 var io = require('socket.io')(server);
 app.use(function(req, res, next){
   res.io = io;
   next();
 });
+
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
